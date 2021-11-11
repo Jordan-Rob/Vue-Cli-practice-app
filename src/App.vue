ad449ea155b633fe2f26c2507a2ea5f800988a6a
unknown <jordanrob709@gmail.com>
2021-11-11 22:28:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
+  <modal />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components:{ Modal, },
+  data(){
+    return{
+      title:"Hello World"
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add("active")
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
